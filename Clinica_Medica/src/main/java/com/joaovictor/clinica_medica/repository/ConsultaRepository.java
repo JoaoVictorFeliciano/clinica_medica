cdfe8bfdebee80dba375905db065f7ebf33360d7
Lázaro Juan de Lima Araújo <lazarojuan08.dev@gmail.com>
2026-09-22 14:38:24 -0300

    feat: repository completed

--- Clinica_Medica/src/main/java/com/joaovictor/clinica_medica/repository/ConsultaRepository.java
+++ Clinica_Medica/src/main/java/com/joaovictor/clinica_medica/repository/ConsultaRepository.java
@@ -1,4 +1,44 @@
 package com.joaovictor.clinica_medica.repository;
 
+import com.joaovictor.clinica_medica.model.Consulta;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Repository
 public class ConsultaRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void salvar(Consulta consulta) {
+        entityManager.persist(consulta);
+    }
+
+    public Consulta buscarPorId(Long id) {
+        return entityManager.find(Consulta.class, id);
+    }
+
+    public List<Consulta> buscarTodos() {
+        return entityManager
+                .createQuery("SELECT c FROM Consulta c", Consulta.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public void atualizar(Consulta consulta) {
+        entityManager.merge(consulta);
+    }
+
+    @Transactional
+    public void excluir(Long id) {
+        Consulta consulta = buscarPorId(id);
+
+        if (consulta != null) {
+            entityManager.remove(consulta);
+        }
+    }
 }
--- Clinica_Medica/src/main/java/com/joaovictor/clinica_medica/repository/MedicoRepository.java
+++ Clinica_Medica/src/main/java/com/joaovictor/clinica_medica/repository/MedicoRepository.java
@@ -1,4 +1,42 @@
 package com.joaovictor.clinica_medica.repository;
 
+import com.joaovictor.clinica_medica.model.Medico;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
 public class MedicoRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void salvar(Medico medico){
+        entityManager.persist(medico);
+    }
+
+    public Medico buscarPorId(Long id){
+        return entityManager.find(Medico.class, id);
+    }
+
+    public List<Medico> buscarTodos(){
+        return entityManager.createQuery("SELECT m FROM Medico m", Medico.class).getResultList();
+    }
+
+    @Transactional
+    public void atualizar(Medico medico){
+        entityManager.merge(medico);
+    }
+
+    @Transactional
+    public void excluir(Long id){
+        Medico medico = buscarPorId(id);
+
+        if (medico != null){
+            entityManager.remove(medico);
+        }
+    }
 }
